419606f8dc1f8ab02302f5161a120c9d68f85e31
herrn0 <113185361+herrn0@users.noreply.github.com>
2022-11-07 19:56:54 -0600

    Delete Netflix_Rating.sql

--- Netflix_Rating.sql
+++
@@ -1,9 +0,0 @@
-SELECT ntd.title, ntd.cast, nc.genre, nc."Hidden Gem Score", nc."Rotten Tomatoes Score", nc.boxoffice
-FROM netflix_title_data AS ntd
-LEFT JOIN netflix_critic nc
-ON ntd.title = nc.title
-WHERE nc."Rotten Tomatoes Score"  > 70
-AND nc."IMDb Votes" > 500
-AND nc.genre NOT LIKE '%Documentary%'
-AND nc."Country Availability" LIKE '%United States%'
-ORDER BY nc."Hidden Gem Score" DESC;
